f8a5babd61aa3d4b8fb8d20a13487c1cf2dc38e7
Arentios <emarsh@sandwich.net>
2016-07-08 13:48:31 -0400

    Implemented recursive version of NW and pulled common code into parent class

--- GeneSequence/src/main/java/com/arentios/gene/sequence/SequenceConstants.java
+++ GeneSequence/src/main/java/com/arentios/gene/sequence/SequenceConstants.java
@@ -2,9 +2,9 @@ package com.arentios.gene.sequence;
 
 public class SequenceConstants {
 
-	public static final Integer NEEDLEMAN_WUNSCH_MISMATCH_DEFAULT = -5;
+	public static final Integer NEEDLEMAN_WUNSCH_MISMATCH_DEFAULT = 0;
 	public static final Integer NEEDLEMAN_WUNSCH_MATCH_DEFAULT = 1;
-	public static final Integer NEEDLEMAN_WUNSCH_INDEL_DEFAULT = -1;
+	public static final Integer NEEDLEMAN_WUNSCH_INDEL_DEFAULT = 0;
 	
 	public static final Integer SMITH_WATERMAN_MISMATCH_DEFAULT = -1;
 	public static final Integer SMITH_WATERMAN_MATCH_DEFAULT = 2;
