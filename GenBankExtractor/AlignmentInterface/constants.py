607396acf32847723a002d0c241fc9593fde5113
Arentios <emarsh@sandwich.net>
2016-10-13 17:03:10 -0400

    Initial super kludgey proof of concept

---
+++ GenBankExtractor/AlignmentInterface/constants.py
@@ -0,0 +1,8 @@
+'''
+Created on October 6, 2016
+
+@author: Eric Marshall
+'''
+PEOPLE_FILE = 'data/alignment.dat'
+LOG_FILE = 'logs/alignment.log'
+ALIGNMENT_URL =  'http://localhost:8080/GeneSequence/alignment/protein'
